e2a36e9a2c87dd66e110abefed33aafacfe8b7cd
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-13 12:39:37 +0200

    draw a triangle

---
+++ 08.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+let lineCount: number = 4;
+
+let i :number =0;
+
+
+let x :string =" * ";
+let y : string =" * ";
+
+for (i;i<lineCount;i++){
+
+    
+console.log(x);
+
+x = x+y;
+
+}
