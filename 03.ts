458325d3393d6aff3da44a4f0d8ae1065780eb00
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-13 09:20:04 +0200

    the new type script file for conditional excersises

--- 03.ts
+++ 03.ts
@@ -13,3 +13,34 @@ number = 7;
 
 console.log(number);
 
+let a: number = 123;
+let b: number = 526;
+
+console.log(a);
+console.log(b);
+
+let x: number ;
+let y:number ;
+x=a;
+y=b;
+
+a=y;
+b=x;
+
+
+console.log(a);
+console.log(b);
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+let BMI = massInKg/(Math.pow(heightInM,2));
+console.log(BMI);
+
+
+
+
+
+
+
+
