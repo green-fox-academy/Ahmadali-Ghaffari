464b765b2ee6ed391d3b2cc1e54c1a4c479e68c6
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-13 10:11:06 +0200

    Cuboid Vol & Surface

--- 04.ts
+++ 04.ts
@@ -1,2 +1,24 @@
 'use strict';
 
+let a : number = 20;
+let b : number = 55.87;
+let c : number = 20;
+
+let a1 : number = b*c;
+let b1 :number = b*a;
+let c1 :number = a*c;
+
+
+
+let surfaceArea : number = 2*(a1+b1+c1);
+
+console.log(surfaceArea);
+
+let volume : number = a*b*c;
+
+console.log(volume);
+
+
+
+
+
