fe56eeedd97dacf3cbffee67d4c27859c76b5423
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-13 11:10:43 +0200

    Food Snack Logical operatiors

---
+++ 05.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+let hungry: Boolean = false;
+let thirsty: Boolean = false;
+
+if (hungry && thirsty){
+    console.log('So this is the Food Time for you!');
+
+}else if (hungry || thirsty){
+
+    console.log('so Now is the SNACK TIME FOR YOUUUUUUU');
+
+}else{
+
+    console.log('U will have nothing nowwww!');
+}
+
