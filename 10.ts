277d796b16bf8077c87fbe1cc7d006f2e1c6c0d7
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-14 21:17:26 +0200

    I can not more so for now is enough

--- 10.ts
+++ 10.ts
@@ -1,18 +1,58 @@
-//---------------------------The Pyramid -------------------------------------\\
+//--------------------------------The Pyramid -------------------------------------\\
 //------------------------Written by Ahmadali Ghaffari------------------------------\\
+//---------------------________________GREEN FOX_________________====================\\
+
 
 'use strict';
 
 let lineCount : number= 4;
-let i: number= 0;
+let i: number= 1;
+let j: number= 0;
 let space : string =" ";
+let tempBox1: string=" ";
+let k:number=1;
+let lineAnswer :string =" ";
+let tempBoxStar :string ="*";
+let tempLineCountBox :number = lineCount;
+
+
+let tempBox2: string =" ";
 let star : string = "*";
 
-for (i;i <lineCount ;i++){
+
+    for(j;j<lineCount;j++){
+
+tempBox1 = tempBox1+space;
+
+}
+
+tempBox1 = tempBox1+star;
+console.log(tempBox1);
+
+//lineCount=lineCount-1;
+
+    for (i;i <lineCount-1 ;i++){
 
 
+        for (k;k<lineCount;k++){
+
+        tempLineCountBox = tempLineCountBox -1;    
+            
+            tempBox2=tempBox2+space;
+
+
+            if(tempLineCountBox=1)
+            {
+                tempBoxStar= star+tempBoxStar+star;
+
+            }
+
+            lineAnswer = tempBox2+tempBoxStar;
+            console.log(lineAnswer);
+
+        }
+
+    }
 
 
     
-}
-
