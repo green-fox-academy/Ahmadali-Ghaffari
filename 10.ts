81c915399769876fad36d400a69b12b077d9d2c7
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-14 18:56:31 +0200

    this is the start of Pyramid

---
+++ 10.ts
@@ -0,0 +1,18 @@
+//---------------------------The Pyramid -------------------------------------\\
+//------------------------Written by Ahmadali Ghaffari------------------------------\\
+
+'use strict';
+
+let lineCount : number= 4;
+let i: number= 0;
+let space : string =" ";
+let star : string = "*";
+
+for (i;i <lineCount ;i++){
+
+
+
+
+    
+}
+
