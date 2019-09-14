a3b25f678e31469e52c7b78e6370b06c144f8c22
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-14 22:35:02 +0200

    The chess Table

---
+++ chessTable.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+let chessLine :string = "% % % %";
+let chessLine2 :string;
+let space: string=" ";
+
+let i :number= 0;
+
+space=space+space+space+space;
+
+console.log(space);
+
+//chessLine2 = space +chessLine;
+chessLine2 = " % % % %";
+for(i;i<4;i++){
+
+    console.log(chessLine);
+    console.log(chessLine2);
+
+
+
+}
