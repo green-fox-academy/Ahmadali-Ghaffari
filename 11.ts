4acfa429f821dd0bd00841da3ef411d450f88553
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-14 21:41:32 +0200

    The Square

---
+++ 11.ts
@@ -0,0 +1,28 @@
+//---------------------Square---------------------\\
+//--------------AHMADALI GHAFFARI------------------\\
+//____________________Green FOX____________________\\
+
+
+'use strict';
+
+let lineCount :number =6;
+
+let x :string= "%%%%%%";
+let y :string= "%";
+let space:string= " ";
+let i:number= 0;
+let counter1: number=lineCount-2;
+
+console.log(x);
+
+
+for (i;i<counter1;i++){
+
+    console.log("%    %");
+
+}
+
+console.log(x);
+
+
+
