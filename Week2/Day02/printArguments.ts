29774f1ac2f77d1e705948ebddb30531b4a545d0
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-17 14:32:29 +0200

    print the Arguments

---
+++ Week2/Day02/printArguments.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+let inputParameters : string[] = ["Hi","Ahmadali","Green Fox"];
+
+function printParams (aArguments : string[] ): string[]{
+
+
+
+return aArguments;
+
+} 
+
+console.log(printParams(inputParameters));
