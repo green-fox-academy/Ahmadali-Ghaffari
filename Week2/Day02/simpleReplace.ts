c6c3f9b3836741e3e822c403e3e1c8c8bc68cdf3
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-17 15:22:10 +0200

    simple Replace

---
+++ Week2/Day02/simpleReplace.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+/*let str : string = "visit Microsoft!";
+
+let res  = str.replace(/microsoft/i ,"Green Fox");
+
+
+
+
+console.log(res);
+*/
+
+let example: string = 'In a dishwasher far far away';
+
+let replace = example.replace(/dishwasher/i ,"galaxy");
+
+console.log(replace);
+
+
+
+
+
+
+
