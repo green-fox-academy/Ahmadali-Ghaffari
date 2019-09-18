6d36a9dbeb0889fdb26123352426085507741519
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-18 12:26:09 +0200

    Added text for TakesLonger String

---
+++ Week2/Day03/takesLonger.ts
@@ -0,0 +1,10 @@
+
+'use strict';
+
+let quote: string = `Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.`;
+
+let Added: string = quote.replace("It ","It always takes longer than ")
+
+console.log(Added);
+
+
