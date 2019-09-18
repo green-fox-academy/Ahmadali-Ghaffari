7b53d05af35c9c465ad4edaae937b107ef0c1f21
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-18 11:39:04 +0200

    Updated Version with two methods

--- Week2/Day03/AnotherReverse.ts
+++ Week2/Day03/AnotherReverse.ts
@@ -1,8 +1,23 @@
 'use strict';
 
 let newString : string= "ahmadali";
-let splitArray : string[]= newString.split("");
+//let splitArray : string[]= newString.split("");
+
+//let reversedArray :string[]= splitArray.reverse();
+
+//console.log(reversedArray);
+
+////////////////////////////////////////////////
+
+function reversingFunction (input : string):string []{
+
+
+    let splitArray : string[]= input.split("");
 
 let reversedArray :string[]= splitArray.reverse();
 
-console.log(reversedArray);
+return reversedArray;
+
+}
+
+console.log(reversingFunction(newString));
