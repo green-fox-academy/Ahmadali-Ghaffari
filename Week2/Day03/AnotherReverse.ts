671e5a818343a1f22ff91395ab4bc9d7bf630638
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-18 11:33:35 +0200

    It is Another Reverse method

---
+++ Week2/Day03/AnotherReverse.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let newString : string= "ahmadali";
+let splitArray : string[]= newString.split("");
+
+let reversedArray :string[]= splitArray.reverse();
+
+console.log(reversedArray);
