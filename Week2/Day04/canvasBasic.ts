504ba8fbfe70577d86a92ebb26870d6211515aa1
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-19 11:40:56 +0200

    Canvas basics

---
+++ Week2/Day04/canvasBasic.ts
@@ -0,0 +1,51 @@
+'use strict';
+
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+
+
+const ctx = canvas.getContext("2d");
+
+//colored Rextangle............................
+//ctx.fillStyle = 'blue';
+//ctx.fillRect (100,100,20,30);
+
+//Draw a Line..................................
+
+/*ctx.beginPath();
+ctx.moveTo(210,210);
+ctx.lineTo(300,300);
+ctx.stroke();
+
+ctx.strokeStyle='green';
+ctx.beginPath();
+ctx.moveTo(150,23);
+ctx.lineTo(450,24);
+ctx.stroke();
+
+ctx.strokeStyle="purple";
+ctx.beginPath();
+ctx.moveTo(27,27);
+ctx.lineTo(550,550);
+ctx.stroke();
+
+*/
+
+//draw circle.....................................
+
+/*ctx.beginPath();
+ctx.arc(200,200,75,60,Math.PI*2.5);
+ctx.stroke();
+*/
+
+
+
+
+
+
+
+
+
+
+
+
