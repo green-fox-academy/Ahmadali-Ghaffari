d98c21a6679f9305fcf8cc0b212b576d79583daa
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-19 13:56:30 +0200

    The Green centered Square

---
+++ Week2/Day04/centered-square.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "green";
+ctx.fillRect(300,200,10,10);
+
+
+
