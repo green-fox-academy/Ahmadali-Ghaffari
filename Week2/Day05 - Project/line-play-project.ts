f25b7f37ad2dcd763855acfa900106d22e712ac6
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-24 20:38:34 +0200

    THE FINAL VERSION

--- Week2/Day05 - Project/line-play-project.ts
+++ Week2/Day05 - Project/line-play-project.ts
@@ -5,16 +5,21 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 //function greenLines(x1: number[] = [0, 0], y1: number[] = [0, 400]) {
-function greenLines(x1: number[] = [], y1: number[] = []) {
+function greenLines() {
+    let greenInitialX: number = 0;
 
+    let greenEndY: number = 0;
+
+    let greenEndX: number=0;
+    let greenInitialY: number=400;
 
     for (let j = 0; j < 19; ++j) {
         ctx.strokeStyle = "green";
         ctx.beginPath();
-        x1[1] = x1[1] + 30;
-        ctx.moveTo(x1[1], y1[0]);
-        y1[1] = y1[1] - 20;
-        ctx.lineTo(x1[0], y1[1]);
+        greenInitialX = greenInitialX + 30;
+        ctx.moveTo(greenInitialX, greenInitialY);
+        greenEndY = greenEndY + 20;
+        ctx.lineTo(greenEndX, greenEndY);
         ctx.stroke();
 
 
@@ -23,35 +28,35 @@ function greenLines(x1: number[] = [], y1: number[] = []) {
 }
 
 //function purpleLines(x2: number[] = [600,600], y2: number[]=[400,0]) {
-function purpleLines(x2: number[] = [], y2: number[] = []) {
+function purpleLines() {
+    let purpleInitialX: number=600;
+    let purpleEndY: number=0;
+
+    let purpleEndX: number= 600;
+    let purpleInitialY: number= 400;
 
 
-    for (let i = 0; i < 19; ++i) 
-    {
+    for (let i = 0; i < 19; ++i) {
+
         ctx.strokeStyle = "purple";
         ctx.beginPath();
-        y2[1] = y2[1] + 20;
-        ctx.moveTo(x2[0], y2[1]);
-        x2[1] = x2[1] - 30;
-        ctx.lineTo(x2[1], y2[0]);
+        purpleInitialY = purpleInitialY - 20;
+        ctx.moveTo(purpleInitialX, purpleInitialY);
+
+        purpleEndX = purpleEndX - 30;
+        ctx.lineTo(purpleEndX, purpleEndY);
         ctx.stroke();
 
 
     }
 }
 
-greenLines([0, 0], [0, 400]);
-purpleLines([600, 600], [400, 0]);
+
+greenLines();
+purpleLines();
 
 
 
-
-//let xy: number[][] = [[20, 40], [200, 70], [95, 150]]
-//for (let j: number = 0; j < 3; ++j) {
-
-  //  horizontalLine(xy[j][0],
-    //    xy[j][1]);
-//}
 
 
 
