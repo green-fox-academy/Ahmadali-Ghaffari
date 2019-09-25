43de14d9b70e2828c3756497d14d2d1208b7dd4f
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-25 16:39:08 +0200

    Basics of Classes

---
+++ Week3/Day03/Human-class.ts
@@ -0,0 +1,78 @@
+'use strict'
+
+class Human {
+
+    name: string;
+    age: number;
+    iq: number;
+
+    constructor(name: string = "Ahmadali", age: number = 0, iq: number = 0) {
+
+        this.name = name;
+        this.age = age;
+        this.iq = iq;
+
+    }
+
+    public isSmart(): boolean {
+
+        return this.iq > 100;
+    }
+
+    public isNameless(): boolean {
+
+        return this.name === 'unknown' || this.name === '';
+
+    }
+
+    public besmarter(): number {
+
+        this.iq += 20;
+        return human.iq;
+
+    }
+}
+
+
+
+let human = new Human();
+//console.log(human.name);
+
+
+let isSmartOUTPUT = human.isSmart();
+
+//console.log(isSmartOUTPUT);
+
+let isnameLESS = human.isNameless();
+
+//console.log(isnameLESS);
+
+//human.besmarter();
+
+//console.log(human.iq);
+
+human.name = "ahmadali The KNIGHT";
+
+human.age = 34;
+
+
+console.log(human.age, human.name);
+
+human.iq = 210;
+
+console.log(human.iq);
+
+
+if (human.isSmart()) {
+ 
+    console.log(human.name + "  is smart , with iq :" + human.iq);
+
+    console.log(`${human.name}  is smart , with iq :${human.iq}`);
+
+}
+
+
+
+
+
+
