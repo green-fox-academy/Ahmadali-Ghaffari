b270131575955008c065651e7badb8fdf7ed1c2d
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-09-26 15:04:31 +0200

    The pokemon of Ash

---
+++ Week3/Day04/Pokemon.ts
@@ -0,0 +1,26 @@
+'use strict'
+
+
+
+
+export class Pokemon {
+
+    private _name: string;
+    private _type: string;
+    private _effective: string;
+
+    constructor(name:string , type:string, effective:string) {
+
+
+        this._name=name;
+        this._type=type;
+        this._effective=effective;
+        1
+    }
+
+    
+
+
+
+
+}
