32fcba3831d1dc0df30c848f196d1b4e64186dbc
Ahmadali <ahmadali.ghaffari@gmail.com>
2019-10-07 22:40:17 +0200

    The pirate Class of Workshop is complete

---
+++ Week5/foundation-Exam-training-Day01/Pirate - exercise/Pirate.ts
@@ -0,0 +1,55 @@
+
+'use strict';
+
+class Pirate {
+
+    private _drinkSomeRum: boolean = false;
+    private _death: boolean = false;
+
+
+    public drinkSomeRum() {
+
+        if (this._death == false) {
+
+            this._drinkSomeRum = true;
+
+        } else {
+
+            console.log("The pirate is Dead !!!!!");
+
+        }
+    }
+
+    public howsItGoingMate() {
+
+        if (this._drinkSomeRum && this._death == false) {
+
+            for (let i = 0; i < Math.floor(Math.random() * 5); i++) {
+
+                console.log("Pour me anudder!");
+            }
+
+        } else if (this._drinkSomeRum == false && this._death == false) {
+
+            console.log("Arghh, I'ma Pirate. How d'ya d'ink its goin?")
+
+        } else{
+
+            console.log("The Pirate is DEAD !!!!!")
+        }
+    }
+
+    public die() {
+
+        this._death = true;
+
+    }
+
+}
+
+let pirate = new Pirate;
+
+//pirate.die();
+
+//pirate.drinkSomeRum();
+pirate.howsItGoingMate();
